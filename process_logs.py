c8b81700c3f244a600044d961d588ba2c1666e0e
Josh Pyorre <jpyorre@opendns.com>
2016-10-28 18:13:25 -0700

    Fixed a problem

--- process_logs.py
+++ process_logs.py
@@ -3,6 +3,8 @@
 import time, sys
 from tldextract import tldextract
 from pymongo import MongoClient
+from netaddr import valid_ipv4
+from netaddr import valid_ipv6
 
 input_file = sys.argv[1]
 
@@ -28,14 +30,20 @@ with open(input_file, 'r') as f:
 		line = line.strip()	# get rid of extra blank lines
 		linesplit = line.split(' ')	
 
-		# Assign each column data to a variable:
-		datetime = linesplit[0] + ' ' + linesplit[1]
-		if len(linesplit) > 11:
-			dom = linesplit[9]
-			domain = tldextract.extract(dom)
-			subd = domain.subdomain
-			line = ("{0},{1}".format(datetime, subd))
-			#print line
-			timeanddomainslist.append(line)
+		try:
+			# Assign each column data to a variable:
+			datetime = linesplit[0] + ' ' + linesplit[1]
+			if len(linesplit) > 11:
+				dom = linesplit[9]
+				domain = tldextract.extract(dom)
+				subd = domain.subdomain
+				temp = ("{0}.{1}").format(subd, domain.domain) # Fix an issue where the last octet of IP's were getting removed
+				temp = temp.rstrip('.,')
+				
+				line = ("{0},{1}".format(datetime, temp))
+				print line
+				timeanddomainslist.append(line)
+		except:
+			continue
 
 put_data_into_mongodb('tcpdumpdns','connections')
